1fd20b2b880f3dcb3cdca245ea6b7d9777e5ecfd
Jez Higgins <jez@jezuk.co.uk>
2017-08-07 10:01:33 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import sys
+
+try:
+    from distutils.core import setup
+except:
+    if sys.version[0] < 2:
+        print "jabber.py requires at least python 2.0"
+        print "Setup cannot continue."
+        sys.exit(1)
+    print "You appear not to have the Python distutils modules"
+    print "installed. Setup cannot continue."
+    print "You can manually install eliza.py by copying eliza.py"
+    print "to your /python-libdir/site-packages directory."
+    sys.exit(1)
+    
+setup(name="eliza.py",
+      version="0.1",
+      py_modules=["eliza"],
+      description="Cheesy Eliza knock-off  ",
+      author="Joe Strout, Jeff Epler and Jez Higgins",
+      author_email="jez@jezuk.co.uk",
+      url="http://www.jezuk.co.uk/eliza",
+      license="BSD style"
+      )
+
+
+
+
+
+
